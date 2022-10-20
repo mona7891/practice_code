d2135ba84fc04b265fcc834c39eea38d1285afce
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-10-20 22:24:25 +0530

    Add files via upload

---
+++ Bubble_sort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Bubble_sort
+{
+	public static void PrintArr(int arr[])
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		int arr[] = {2,3,4,5,1,20};
+
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j] > arr[j+1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+
+		}
+		PrintArr(arr);
+	}
+}
