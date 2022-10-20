7e79846c190d1cd0fed12c5f8d2f987e8a080891
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-10-20 23:36:13 +0530

    Add files via upload

---
+++ Even_Arr.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Even_Arr
+{
+	public static void main(String args[])
+	{
+		int arr[]={1,2,5,6,3,8};
+		
+		System.out.println("Array Element Are :");
+		
+		for(int i=0;i<arr.length;i++)
+		{
+		    if(arr[i]%2==0)
+		    {
+		 	 System.out.print(arr[i]+" ");
+		    }
+	        }
+	}
+}
