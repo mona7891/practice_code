635f032441024737d6ec30ab8536f728d1d016ee
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-10-21 17:30:26 +0530

    Add files via upload

---
+++ While_loop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class While_loop
+{
+	public static void main(String args[])
+	{
+		//while(condition) --> do something 
+		int i=0;
+		
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		System.out.println();
+	}
+}
