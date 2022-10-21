eb07698817b1807a55f64b80a4b3602ce1934264
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-10-21 12:28:07 +0530

    Add files via upload

---
+++ Selection_sort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Selection_sort
+{
+	public static void PrintArray(int arr[])
+	{
+		for(int i=0;i<arr.length; i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		int arr[] = {7,8,3,1,2};
+	
+		
+		//selection sort
+		for(int i=0;i<arr.length-1; i++)
+		{
+			int small =i;
+			for(int j=i+1 ;j<arr.length;j++)
+			{
+				if(arr[small]>arr[j])
+				{
+					small = j;
+				}
+			}
+		
+		// swap
+		int temp = arr[small];
+		   arr[small] = arr[i];
+		   arr[i] = temp; 
+		}
+	PrintArray(arr);
+	}
+
+
+}
