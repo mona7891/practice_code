d81be37fc45da5484e3724be76f60fae61e070dd
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-10-21 16:03:44 +0530

    Add files via upload

---
+++ Else_if.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+class Else_if
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		if(a==b)
+		{
+			System.out.println("A AND B ARE EQUAL !!");
+		}
+		else if(a>b)
+		{
+			System.out.println("A IS  GHETER THAN B  !!");
+		}
+		else
+		{
+			 System.out.println("A IS LESSER THAN B  !!");
+
+		}
+		
+	}
+}
