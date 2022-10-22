e7e337e855aa2ddec7d3fda135542f6c8fbdb2e5
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-10-22 21:58:06 +0530

    Add files via upload

---
+++ Solid_Rectangle.java
@@ -0,0 +1,28 @@
+/*  Solid Rectangle.java 
+*****
+*****
+*****
+*****
+*/    
+
+import java.util.*;
+class Solid_Rectangle
+{
+	public static void main(String args[])
+	{
+		int n = 4;
+		int m = 5;
+
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=m;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
